f5e04a13f774d5bd5280641cbf5c9ae80088ce82
Jeong tae yeon <jtyk119@naver.com>
2023-09-29 18:18:43 +0900

    랜덤 위치에 손 생성

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import random
 
 TUK_WIDTH, TUK_HEIGHT = 800, 600
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
@@ -7,6 +8,9 @@ TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 hand = load_image('hand_arrow.png')
 
+hand_x, hand_y, hand_check, dir_check = random.randint(30, 770), random.randint(30, 670), 1, 0
+def create_hand():
+    hand_x, hand_y, hand_check = random.randint(30, 770), random.randint(30, 670), 1
 
 def handle_events():
     global running
@@ -15,12 +19,8 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-    pass
-
 
 
 running = True
@@ -32,6 +32,7 @@ while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    hand.draw(hand_x, hand_y)
     update_canvas()
     frame = (frame + 1) % 8
 
