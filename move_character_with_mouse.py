405175c15fe0ca3605fcb3a1af989da3b09e2858
Jeong tae yeon <jtyk119@naver.com>
2023-09-30 00:12:45 +0900

    Drill05 제출 (2022182037 정태연)

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -8,9 +8,14 @@ TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 hand = load_image('hand_arrow.png')
 
-hand_x, hand_y, hand_check, dir_check = random.randint(30, 770), random.randint(30, 670), 1, 0
+hand_x, hand_y, hand_check, dir_check = 0,0, False,0
 def create_hand():
-    hand_x, hand_y, hand_check = random.randint(30, 770), random.randint(30, 670), 1
+    global x, hand_x, hand_y, hand_check, dir_check
+    hand_x, hand_y, hand_check = random.randint(100, 700), random.randint(100, 600), True
+    if x > hand_x:
+        dir_check = 0
+    else:
+        dir_check = 1
 
 def handle_events():
     global running
@@ -22,20 +27,35 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
-
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-frame = 0
+save_x, save_y = x, y
+i, frame = 0, 0
 hide_cursor()
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    if hand_check == False:
+        create_hand()
+
+    if dir_check == 0:
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+    elif dir_check == 1:
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+
+    t = i / 500
+    x = (1 - t) * save_x + t * hand_x
+    y = (1 - t) * save_y + t * hand_y
+    i = i + 1
+    if i == 500:
+        i = 0
+        save_x, save_y = x, y
+        hand_check = False
+
     hand.draw(hand_x, hand_y)
     update_canvas()
     frame = (frame + 1) % 8
-
     handle_events()
 
 close_canvas()
